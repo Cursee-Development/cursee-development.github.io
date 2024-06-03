aa38e04d6ce1514c1bb52379d1cecd2374e7c791
Sinclair <truly.sinclair@gmail.com>
2024-06-02 21:06:58 -0400

    Add React integration to Astro project
    The commit includes adding React to the Astro project. The React dependency has been added to package.json along with the corresponding TypeScript definitions. The configurations for the Astro project and the TypeScript compiler options have been updated to enable the integration of React.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cursee-development.github.io/",
   base: '/cursee-development',
+  integrations: [react()]
 });
